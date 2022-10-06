14c54a87cedd62c5d30358b62fa5f0d325f9de0e
Oleh <oleg_bob@ukr.net>
2022-10-06 17:41:25 +0300

    Auto login-logout added

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {HttpClient, HttpHeaders, HttpParams} from "@angular/common/http";
+import {HttpClient} from "@angular/common/http";
 import {Recipe} from "../recipes/recipe.model";
 import {RecipeService} from "../services/recipe.service";
 import {map, tap} from "rxjs";
