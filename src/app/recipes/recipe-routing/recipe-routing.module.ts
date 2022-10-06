c948b58915b4f0b8a39e2f9f8623152a2946f0e8
Oleh <oleg_bob@ukr.net>
2022-10-06 18:00:48 +0300

    Auth guard added

--- src/app/recipes/recipe-routing/recipe-routing.module.ts
+++ src/app/recipes/recipe-routing/recipe-routing.module.ts
@@ -6,9 +6,12 @@ import {RecipeDetailComponent} from "../recipe-detail/recipe-detail.component";
 import {RecipeStartComponent} from "../recipe-start/recipe-start.component";
 import {RecipeResolver} from "../../services/recipe.resolver";
 import {RecipeEditComponent} from "../recipe-edit/recipe-edit.component";
+import {AuthGuard} from "../../auth/auth.guard";
 
 const recipeRoutes: Routes = [
-  {path: '', component: RecipesComponent, children: [
+  {path: '', component: RecipesComponent,
+    canActivate: [AuthGuard],
+    children: [
       {path: '', component: RecipeStartComponent},
       {path: 'new', component: RecipeEditComponent},
       {path: ':id', component: RecipeDetailComponent, resolve: [RecipeResolver]},
